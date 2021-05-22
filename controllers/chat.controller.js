a1072af6a3ac0fd97a8f95f50267a8b9ec53abed
atul-gairola <atulgairola08@gmail.com>
2021-05-22 17:01:05 +0530

    Refactoring

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,3 +1,6 @@
+const { generateLogger, getCurrentFilename } = require("../logger");
+const logger = generateLogger(`Routes : ${getCurrentFilename(__filename)}`);
+
 const ChatModel = require("../db/models/ChatModel");
 const MessageModel = require("../db/models/MessageModel");
 const UserModel = require("../db/models/UserModel");
@@ -7,59 +10,69 @@ exports.createNewChatController = async (req, res) => {
 
   const { uid } = params;
 
-  // check if the current user is only the authenticated user
-  if (uid !== currentUser.uid) {
-    logger.error(`Authenticated user not allowed access`);
-    return res.status(403).json({ message: "Forbidden" });
-  }
-
-  //   if no content was sent in the message return
-  if (!body.message || body.message.trim() === "") {
-    return res.status(201).json({ message: "No message recieved" });
-  }
-
-  // check if a chat exists between the two users
-  const chatExists = await ChatModel.findOne({
-    members: { $all: [currentUser._id, body.to] },
-  });
-
-  if (chatExists) {
-    return res
-      .status(422)
-      .json({ message: "Chat already exists", chatId: chatExists._id });
-  }
-
-  const newMessage = new MessageModel({
-    from: currentUser._id,
-    to: [body.to],
-    content: body.message,
-  });
-
-  const createdChat = await ChatModel.create({
-    members: [currentUser._id, body.to],
-    messages: [newMessage._id],
-    lasMessage: newMessage._id,
-    createdBy: body.from,
-  });
-
-  newMessage.chatID = createdChat._id;
-
-  await newMessage.save();
-
-  //   update both users
-  const updatedUsers = await UserModel.updateMany(
-    { _id: { $in: [currentUser._id, body.to] } },
-    {
-      $push: { chatIds: createdChat._id },
-    },
-    {
-      new: true,
+  try {
+    // check if the current user is only the authenticated user
+    if (uid !== currentUser.uid) {
+      logger.error(`Authenticated user not allowed access`);
+      return res.status(403).json({ message: "Forbidden" });
     }
-  );
 
-  res.json({
-    newMessage,
-    createdChat,
-    updatedUsers,
-  });
+    //   if no content was sent in the message return
+    if (!body.message || body.message.trim() === "") {
+      return res.status(201).json({ message: "No message recieved" });
+    }
+
+    // check if a chat exists between the two users
+    const chatExists = await ChatModel.findOne({
+      members: { $all: [currentUser._id, body.to] },
+    });
+
+    if (chatExists) {
+      return res
+        .status(422)
+        .json({ message: "Chat already exists", chatId: chatExists._id });
+    }
+
+    const newMessage = new MessageModel({
+      from: currentUser._id,
+      to: [body.to],
+      content: body.message,
+    });
+
+    const createdChat = await ChatModel.create({
+      members: [currentUser._id, body.to],
+      messages: [newMessage._id],
+      lastMessage: newMessage._id,
+      createdBy: body.from,
+    });
+
+    newMessage.chatID = createdChat._id;
+
+    await newMessage.save();
+
+    //   update both users
+    await UserModel.updateMany(
+      { _id: { $in: [currentUser._id, body.to] } },
+      {
+        $push: { chatIds: createdChat._id },
+      }
+      // {
+      //   new: true,
+      // }
+    );
+
+    return res.json({
+      message: "New Chat created",
+      chat: {
+        chatId: createdChat._id,
+        lastMessage: createdChat.lastMessage,
+        messages: createdChat.messages,
+      },
+    });
+  } catch (e) {
+    logger.error(`Error in creating new chat : ${e}`);
+    return res
+      .status(500)
+      .json({ message: "Internal server error. Please try after some time." });
+  }
 };
--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,6 +1,8 @@
+const { generateLogger, getCurrentFilename } = require("../logger");
+const logger = generateLogger(`Routes : ${getCurrentFilename(__filename)}`);
+
 const ChatModel = require("../db/models/ChatModel");
 const MessageModel = require("../db/models/MessageModel");
-const UserModel = require("../db/models/UserModel");
 
 exports.sendMessageController = async (req, res) => {
   const { currentUser, params, body } = req;
@@ -14,45 +16,51 @@ exports.sendMessageController = async (req, res) => {
   //   return res.status(403).json({ message: "Forbidden" });
   // }
 
-  console.log(params);
+  // console.log(params);
+  try {
+    // get the chat
+    const currentChat = await ChatModel.findById(chatId);
 
-  // get the chat
-  const currentChat = await ChatModel.findById(chatId);
+    // check if chat exists
+    if (!currentChat) {
+      return res.status(404).json({ message: "Chat not found" });
+    }
 
-  // check if chat exists
-  if (!currentChat) {
-    return res.status(404).json({ message: "Chat not found" });
+    // check to ensure message is not empty
+    if (!body.message || body.message.trim() === "") {
+      return res.status(201).json({ message: "No message recieved" });
+    }
+
+    const recieverList = currentChat.members.filter(
+      (cur) => String(cur) !== String(currentUser._id)
+    );
+
+    console.log(recieverList, typeof String(currentUser._id));
+
+    // save the new message
+    const savedMessage = await MessageModel.create({
+      from: currentUser._id,
+      chatID: chatId,
+      to: recieverList,
+      content: body.message,
+      status: "SENT",
+    });
+
+    // reflect the message on the users
+    await ChatModel.findByIdAndUpdate(
+      chatId,
+      {
+        $push: { messages: savedMessage._id },
+        lastMessage: savedMessage._id,
+      },
+      { new: true }
+    );
+
+    return res.json({ message: "Message Sent", status: "SENT" });
+  } catch (e) {
+    logger.error(`Error in creating new chat : ${e}`);
+    return res
+      .status(500)
+      .json({ message: "Internal server error. Please try after some time." });
   }
-
-  // check to ensure message is not empty
-  if (!body.message || body.message.trim() === "") {
-    return res.status(201).json({ message: "No message recieved" });
-  }
-
-  const recieverList = currentChat.members.filter(
-    (cur) => String(cur) !== String(currentUser._id)
-  );
-
-  console.log(recieverList, typeof String(currentUser._id));
-
-  // save the new message
-  const savedMessage = await MessageModel.create({
-    from: currentUser._id,
-    chatID: chatId,
-    to: recieverList,
-    content: body.message,
-    status: "SENT",
-  });
-
-  // reflect the message on the users
-  const updatedChat = await ChatModel.findByIdAndUpdate(
-    chatId,
-    {
-      $push: { messages: savedMessage._id },
-      lastMessage: savedMessage._id,
-    },
-    { new: true }
-  );
-
-  return res.json({ savedMessage, updatedChat, recieverList });
 };
